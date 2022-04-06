9369f039701cd77b553714dfd1a2933cdf45604a
Arl33t <arl33t@gmail.com>
2022-04-06 15:09:03 +0200

    basic name search from file
    currently only logs to console

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
+// Load json from file
+let pep;
+fetch("pep_small.json")
+  .then(response => response.json())
+  .then(data => pep = data)
+  .then(() => console.log(pep));
+
+
 // Search function. Runs on button click or enter key.
 function search(event) {
-    text = document.getElementById("searchInput").value
+    text = document.getElementById("searchInput").value;
+    tableCont = document.getElementById("main-table-cont");
+    table = document.getElementById("main-table");
+    nameFound = false;
+    
     if (event.key == "Enter" || event == "btn") {
-        console.log(text)
+        for (const key in pep) {
+            let name = pep[key]["name"];
+            let alias = pep[key]["aliases"].split(";");
+
+            if (text == name || alias.includes(text)) {
+                console.log(pep[key]);
+                nameFound = true;
+            }
+        }
+        if (!nameFound) {
+        console.log("Name not found");
+        }
     }
 }
