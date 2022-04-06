24a23895d77446794a8d5ff6fff37626d339d9f9
Arl33t <arl33t@gmail.com>
2022-04-06 17:46:44 +0200

    name search from api call, loading animation
    only outputs to console

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-// Load json from file
-let pep;
-fetch("pep_small.json")
-  .then(response => response.json())
-  .then(data => pep = data)
-  .then(() => console.log(pep));
+// Variable declaration. Some on window load to catch DOM Elements
+window.onload = function() {
+    const tableCont = document.getElementById("main-table-cont");
+    const table = document.getElementById("main-table");
+    const tableBody = document.getElementById("tablebody");
+    const tableMeta = document.getElementById("tableMeta");
+    const loading = document.getElementById("loading");
+}
+
+// Loading animation functions
+function showLoading() {
+    loading.style.display = "flex";
+}
+function hideLoading() {
+    loading.style.display = "none";
+}
+
+
+// API call. Returns data from API.
+let info; // Variable for fetched data
+async function fetchInfo(name) {
+    showLoading();
+    let url = "https://code-challenge.stacc.dev/api/pep?name=" + name;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    hideLoading();
+    console.log("Data received");
+    info = data;
+    console.log(data);
+
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         hideLoading()
+    //         console.log("Data received")
+    //         info = data
+    //     });
+    return data;
+}
 
 
 // Search function. Runs on button click or enter key.
-function search(event) {
-    text = document.getElementById("searchInput").value;
-    tableCont = document.getElementById("main-table-cont");
-    table = document.getElementById("main-table");
-    nameFound = false;
-    
-    if (event.key == "Enter" || event == "btn") {
-        for (const key in pep) {
-            let name = pep[key]["name"];
-            let alias = pep[key]["aliases"].split(";");
+async function search(event) {
+    input = document.getElementById("searchInput").value;
 
-            if (text == name || alias.includes(text)) {
-                console.log(pep[key]);
-                nameFound = true;
-            }
-        }
-        if (!nameFound) {
-        console.log("Name not found");
-        }
+    if (event.key == "Enter" || event == "btn") {
+        const response = await fetchInfo(input);
+        //console.log(response);
     }
 }
